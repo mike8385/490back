4551ff5de26484d0ecc2b08e445abdd320b77904
mike8385 <146226367+mike8385@users.noreply.github.com>
2025-03-14 17:54:10 -0400

    Milestone3

--- flask-server/models.py
+++ flask-server/models.py
@@ -1,5 +1,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
+from geoalchemy2 import Geometry  # Import Geometry type
+
 
 db = SQLAlchemy()
 
@@ -31,7 +33,12 @@ class Film(db.Model):
 class Rental(db.Model):
     __tablename__ = 'rental'
     rental_id = db.Column(db.Integer, primary_key=True)
-    inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.inventory_id'))
+    rental_date = db.Column(db.DateTime, nullable=False)  # Rental timestamp
+    inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.inventory_id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'), nullable=False)
+    return_date = db.Column(db.DateTime, nullable=True)  # Nullable, since some may not be returned yet
+    staff_id = db.Column(db.Integer)#, db.ForeignKey('staff.staff_id'), nullable=False)
+    last_update = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
 class Inventory(db.Model):
     __tablename__ = 'inventory'
@@ -56,7 +63,78 @@ class Actor(db.Model):
    # category_id = db.Column(db.Integer, primary_key=True)
     #name = db.Column(db.String(25))
 
+class Address(db.Model):
+    __tablename__ = 'address'
+    address_id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(50))
+    address2 = db.Column(db.String(50), nullable = True)
+    district = db.Column(db.String(20))
+    city_id = db.Column(db.Integer, db.ForeignKey('city.city_id'))
+    phone = db.Column(db.String(20))
+    location = db.Column(Geometry("POINT"), nullable=True)  # Add location column
+
+
 
     #Create A string
   #  def __repr__(self):
  #       return '<Name %r>' % self.name
+
+class Customer(db.Model):
+    __tablename__ = 'customer'
+    customer_id = db.Column(db.Integer, primary_key=True)
+    store_id = db.Column(db.Integer,db.ForeignKey('store.store_id'))
+    first_name = db.Column(db.String(45), nullable=False)
+    last_name = db.Column(db.String(45), nullable=False)
+    email = db.Column(db.String(50), nullable=True)
+    address_id = db.Column(db.Integer, db.ForeignKey('address.address_id'))
+    active = db.Column(db.Integer())
+    #create_date = db.Column(db.Date)
+ #   class Category(db.Model):
+
+class Country(db.Model):
+    __tablename__ = 'country'
+    country_id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String(50), nullable=False)
+    last_update = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+class City(db.Model):
+    __tablename__ = 'city'
+    city_id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(50), nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.country_id'), nullable=False)
+    last_update = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+
+
+class Store(db.Model):
+    __tablename__ = 'store'
+    store_id = db.Column(db.Integer, primary_key=True)  # TINYINT in MySQL
+    manager_staff_id = db.Column(db.Integer, db.ForeignKey('staff.staff_id'), nullable=False)
+    address_id = db.Column(db.Integer, db.ForeignKey('address.address_id'), nullable=False)
+
+
+class Staff(db.Model):
+    __tablename__ = 'staff'
+    staff_id = db.Column(db.Integer, primary_key=True)  # TINYINT in MySQL
+    first_name = db.Column(db.String(45), nullable=False)
+    last_name = db.Column(db.String(45), nullable=False)
+    address_id = db.Column(db.Integer, db.ForeignKey('address.address_id'), nullable=False)
+    email = db.Column(db.String(50), nullable=True)
+    store_id = db.Column(db.Integer, db.ForeignKey('store.store_id'), nullable=False)
+    active = db.Column(db.Boolean, nullable=False, default=True)  # TINYINT(1) as Boolean
+    username = db.Column(db.String(16), nullable=False, unique=True)
+    password = db.Column(db.String(40), nullable=False)  # Hashing recommended in actual use
+    last_update = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+
+
+# class Address(db.Model):
+#     __tablename__ = 'address'
+#     address_id = db.Column(db.Integer, primary_key=True)
+#     store_id = db.Column(db.Integer,db.ForeignKey('store.store_id'))
+#     first_name = db.Column(db.String(45))
+#     last_name = db.Column(db.String(45))
+#     email = db.Column(db.String(50))
+#     address_id = db.Column(db.Integer, db.ForeignKey('address.address_id'))
+#     active = db.Column(db.Integer(1))
+#     create_date = db.Column(db.Date)
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
-from datetime import datetime
 from models import db, FilmCategory, Film  # Import db from models, and necessary models
 from flask_cors import CORS
+import logging;
+from datetime import datetime, timezone
+
 
 
 
@@ -116,6 +118,403 @@ def search_films():
                         })
 
     return jsonify({'films': films})
+
+
+
+@app.route("/searchcustomers", methods=['GET'])
+def search_customers():
+    from models import FilmCategory, Inventory, Film, Category, Actor, Film_Actor, Customer, Rental  # Import Film model from models.py
+
+    customer_list = db.session.query(
+        Customer.customer_id,
+        Customer.first_name,
+        Customer.last_name,
+        func.count(Rental.rental_id).label("count")
+    ).outerjoin(Rental, Rental.customer_id == Customer.customer_id
+    ).group_by(Customer.customer_id
+               ).order_by(Customer.last_name.asc()).all()
+
+
+    
+    customer = []
+
+    for cust in customer_list:
+        customer.append({"customer_id": cust.customer_id,
+                       "first_name": cust.first_name,
+                        "last_name":cust.last_name,
+                        "count" : cust.count,
+
+                        })
+    return jsonify({'customers': customer})
+
+@app.route("/addcustomers", methods=['POST', 'GET'])
+def add_customers():
+    from models import Customer, Address, City, Country# Import Film model from models.py
+
+    data = request.json  # Use JSON data from request body
+
+
+    if request.method == "POST":
+
+            # Check if the city already exists
+        
+#-----------------------------------------------
+        # Check if the address already exists
+
+#----------------------------------------------------------
+        existing_country = Country.query.filter_by(country=data['country']).first()
+        
+        if existing_country:
+            country_id = existing_country.country_id  # Ensure country_id is always set
+        else:
+            new_country = Country(country=data['country'])
+            db.session.add(new_country)
+            db.session.commit()
+            country_id = new_country.country_id  # Get the new city's ID
+#-------------------------------------------
+
+        existing_city = City.query.filter_by(city=data['city']).first()
+        
+        if existing_city:
+            city_id = existing_city.city_id  # Use existing city ID
+        else:
+            new_city = City(city=data['city'], country_id=country_id)
+            db.session.add(new_city)
+            db.session.commit()
+            city_id = new_city.city_id  # Get the new city's ID
+#---------------------------------------------------------
+        existing_address = Address.query.filter_by(address=data['address'], district=data['district']).first()
+
+        if existing_address:
+            new_address = existing_address  # Use existing address ID
+        else:
+            new_address = Address(address=data['address'], district=data['district'], 
+                                  city_id=city_id,
+                                  phone=data.get('phone', '000-000-0000'),  # Provide a default phone number
+                                    location='POINT(0 0)')  # Provide a placeholder location if required
+            db.session.add(new_address)
+            db.session.commit()
+            address_id = new_address.address_id  # Get the new address ID
+#---------------------------------------------------------
+
+        customer = Customer(first_name=data['first_name'], 
+                            last_name=data['last_name'], 
+                            email=data['email'],
+                            address_id=new_address.address_id,
+                            store_id= 1 ,
+                            active=data.get('active', 1))
+
+        db.session.add(customer)
+        db.session.commit()
+
+
+        return jsonify({
+            "success": True,
+            "message": "Customer added successfully!",
+            "customer": {
+                "customer_id": customer.customer_id,
+                "first_name": customer.first_name,
+                "last_name": customer.last_name,
+                "count" : 0
+            }
+        })
+    
+@app.route("/deletecustomers/<int:customer_id>",methods = ['DELETE'])
+def delete_customers(customer_id):
+    from models import Customer, Rental
+    if request.method == 'DELETE':
+        customer = Customer.query.get(customer_id)
+
+
+
+        
+        if not customer:
+            return jsonify({"success": False, "message": "Customer not found"}), 404
+
+        # Delete customer from database
+        db.session.query(Rental).filter(Rental.customer_id == customer_id).delete()
+        db.session.commit()
+        db.session.delete(customer)
+        db.session.commit()
+
+        return jsonify({"success": True, "message": "Customer deleted"}), 200
+
+@app.route("/updatecustomers/<int:customer_id>",methods = ['PUT', 'GET'])
+def update_customers(customer_id):
+    from models import Customer, Address, City, Country
+    customer = Customer.query.get(customer_id)
+    if not customer:
+            return jsonify({"success": False, "message": "Customer not found"}), 404
+    
+    if request.method == 'GET':
+        address = Address.query.get(customer.address_id)
+        city = City.query.get(address.city_id)
+        country = db.session.get(Country, city.country_id)
+        return jsonify({
+            "customer_id": customer.customer_id,
+            "first_name": customer.first_name,
+            "last_name": customer.last_name,
+            "email": customer.email,
+            "address": address.address,
+            "district": address.district,
+            "city": city.city,
+            "country": country.country,
+            "phone": address.phone
+        })
+    
+    data = request.json
+
+        # Update Country if different
+    existing_country = Country.query.filter_by(country=data['country']).first()
+    if not existing_country:
+        new_country = Country(country=data['country'])
+        db.session.add(new_country)
+        db.session.commit()
+        country_id = new_country.country_id
+    else:
+        country_id = existing_country.country_id
+
+    # Update City if different
+    existing_city = City.query.filter_by(city=data['city'], country_id=country_id).first()
+    if not existing_city:
+        new_city = City(city=data['city'], country_id=country_id)
+        db.session.add(new_city)
+        db.session.commit()
+        city_id = new_city.city_id
+    else:
+        city_id = existing_city.city_id
+
+    # Update Address if different
+    existing_address = Address.query.filter_by(address=data['address'], district=data['district']).first()
+    if not existing_address:
+        new_address = Address(
+            address=data['address'], 
+            district=data['district'], 
+            city_id=city_id,
+            phone=data.get('phone', '000-000-0000'),  # Provide a default if missing
+            location='POINT(0 0)'  # Placeholder
+        )
+        db.session.add(new_address)
+        db.session.commit()
+        address_id = new_address.address_id
+    else:
+            existing_address.phone = data.get('phone', existing_address.phone)  # Keep existing if not provided
+            db.session.commit()  # Save changes
+            address_id = existing_address.address_id
+
+    # Update Customer Information
+    customer.first_name = data['first_name']
+    customer.last_name = data['last_name']
+    customer.email = data['email']
+    customer.address_id = address_id
+
+    db.session.commit()
+
+    return jsonify({
+        "success": True,
+        "message": "Customer updated successfully!",
+        "customer": {
+            "customer_id": customer.customer_id,
+            "first_name": customer.first_name,
+            "last_name": customer.last_name,
+            "email": customer.email
+        }
+    }), 200
+
+
+
+@app.route("/rent", methods=["PATCH","POST"])
+def rent_movie():
+    from models import Customer, Rental  # Import necessary models
+    data = request.json
+    customer_id = data.get('customer_id')
+    film_id = data.get('film_id')
+
+    if not customer_id or not film_id:
+        return jsonify({"success": False, "message": "customer_id and film_id are required"}), 400
+
+    try:
+        # Find the customer
+        customer = db.session.get(Customer, customer_id)
+        if not customer:
+            return jsonify({"success": False, "message": "Customer not found"}), 404
+
+        # Increment the customer's rental count
+        #customer.rented_count = (customer.rented_count or 0) + 1
+
+        # Create a new rental record
+        new_rental = Rental(
+            customer_id=customer_id,
+            inventory_id=film_id,  # Assuming film_id maps to inventory_id
+            staff_id=1
+            #rental_date=datetime.now(),
+            #return_date=None  # Set return_date as needed
+        )
+        db.session.add(new_rental)
+
+        # Commit changes to the database
+        db.session.commit()
+
+        return jsonify({"success": True, "message": "Rental recorded successfully!"}), 200
+    except Exception as e:
+        db.session.rollback()  # Rollback in case of error
+        logging.error(f"Error in rent_movie: {str(e)}")  # Log the error
+        return jsonify({"success": False, "message": str(e)}), 500
+
+# @app.route("/rent" ,methods = ["PATCH"])
+# def rent_movie():
+#     from models import Customer, Address, City, Country
+#     data = request.json
+#     customer_id = data['customer_id']
+#     film_id = data['film_id']
+
+#     # Update rented movies count
+#     cursor = db.cursor()
+#     cursor.execute("UPDATE customer SET rented_count = rented_count + 1 WHERE customer_id = %s", (customer_id,))
+#     cursor.execute("INSERT INTO rental (customer_id, film_id, rental_date) VALUES (%s, %s, NOW())", (customer_id, film_id))
+#     db.commit()
+
+#     return jsonify({"success": True})
+
+@app.route("/info/<int:customer_id>",methods = ['PUT', 'GET'])
+def info_customers(customer_id):
+    from models import Customer, Address, City, Country, Rental, Inventory
+    customer = Customer.query.get(customer_id)
+
+
+    if not customer:
+            return jsonify({"success": False, "message": "Customer not found"}), 404
+    
+
+    #Filters a customers id, name, and rental count based off of their id
+    customerData = db.session.query(
+        Customer.customer_id,
+        Customer.first_name,
+        Customer.last_name,
+        func.count(Rental.rental_id).label("count")
+    ).outerjoin(Rental, Rental.customer_id == Customer.customer_id
+    ).group_by(Customer.customer_id
+               ).filter(Customer.customer_id == customer.customer_id  
+               ).order_by(Customer.last_name.asc()).all()
+    
+
+
+    rental_history = db.session.query(
+        Customer.customer_id,
+        Customer.first_name,
+        Customer.last_name,
+        Rental.rental_date,
+        Rental.return_date,
+        Inventory.film_id,
+        func.ifnull(Rental.return_date, "Not Returned").label("status")
+    ).join(Rental, Rental.customer_id == Customer.customer_id
+    ).join(Inventory, Inventory.inventory_id == Rental.inventory_id
+    ).filter(Customer.customer_id == customer_id  
+    ).order_by(Rental.rental_date.desc()
+    ).all()
+
+
+    currently_rented = db.session.query(
+        Rental.rental_id,
+        Rental.rental_date,
+        Inventory.film_id
+    ).join(Inventory, Inventory.inventory_id == Rental.inventory_id
+    ).filter(Rental.customer_id == customer_id, Rental.return_date.is_(None)  
+    ).order_by(Rental.rental_date.desc()
+    ).all()
+
+
+
+    
+    if request.method == 'GET':
+        address = Address.query.get(customer.address_id)
+        city = City.query.get(address.city_id)
+        country = Country.query.get(city.country_id)
+        rented = customerData[0].count if customerData else 0
+        returned = sum(1 for rental in rental_history if rental.return_date is not None)
+
+        rented_movies = [
+            {"rental_id": rental.rental_id, "rental_date": rental.rental_date, "film_id": rental.film_id}
+            for rental in currently_rented
+        ]
+
+        rental_history_list = [
+            {"rental_date": rental.rental_date, "return_date": rental.return_date, "film_id": rental.film_id}
+            for rental in rental_history
+        ]
+
+        
+
+        
+
+        return jsonify({
+            "customer_id": customer.customer_id,
+            "first_name": customer.first_name,
+            "last_name": customer.last_name,
+            "email": customer.email,
+            "address": address.address,
+            "district": address.district,
+            "city": city.city,
+            "country": country.country,
+            "phone": address.phone,
+            "rented": rented,
+            "returned": returned,
+            "rental_history": rental_history_list,
+            "currently_rented": rented_movies
+
+        })
+    
+    data = request.json
+
+        # Update Country if different
+    existing_country = Country.query.filter_by(country=data['country']).first()
+    country_id = existing_country.country_id
+
+    # Update City if different
+    existing_city = City.query.filter_by(city=data['city'], country_id=country_id).first()
+    city_id = existing_city.city_id
+
+    # Update Address if different
+    existing_address = Address.query.filter_by(address=data['address'], district=data['district']).first()
+    address_id = existing_address.address_id
+
+
+
+@app.route("/returnfilm", methods=["PATCH","POST"])
+def return_movie():
+    from models import Customer, Rental  # Import necessary models
+    data = request.json
+    customer_id = data.get('customer_id')
+    film_id = data.get('film_id')
+
+    if not customer_id or not film_id:
+        return jsonify({"success": False, "message": "customer_id and film_id are required"}), 400
+
+    try:
+        # Find the customer
+        customer = db.session.get(Customer, customer_id)
+        if not customer:
+            return jsonify({"success": False, "message": "Customer not found"}), 404
+
+        # Increment the customer's rental count
+        #customer.rented_count = (customer.rented_count or 0) + 1
+
+        rental = Rental.query.filter_by(customer_id=customer_id, 
+                                        return_date=None).first()
+
+        if not rental:
+            return jsonify({"success": False, "message": "No active rental found for this movie"}), 404
+        rental.return_date = datetime.now(timezone.utc)
+
+        # Commit changes to the database
+        db.session.commit()
+
+        return jsonify({"success": True, "message": "Rental recorded successfully!"}), 200
+    except Exception as e:
+        db.session.rollback()  # Rollback in case of error
+        logging.error(f"Error in rent_movie: {str(e)}")  # Log the error
+        return jsonify({"success": False, "message": str(e)}), 500
+
 
 
 if __name__ == "__main__":
